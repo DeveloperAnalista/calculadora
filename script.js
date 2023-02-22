531931b6bfd0812767c45a76c11ac7053d65d7ab
Developer.Analista <110179305+DeveloperAnalista@users.noreply.github.com>
2023-02-22 15:27:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function displaynum(n1) {
     calculator.text1.value = calculator.text1.value + n1;
 };
+valor variável
+var  resultado
+
+função  botao ( num )  {
+	valor  =  documento . calc . viseira . valor  +=  num
+}
+
+função  calcular ( )  {
+	resultado  =  valor ( valor )
+	documento . calc . viseira . valor  =  resultado . toLocaleString ( 'pt-br' )
+};
